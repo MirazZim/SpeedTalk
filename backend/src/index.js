cdb7e2daccf874befded593896b47a3a0be9d2d3
MirazZim <miraz.zim.38@gmail.com>
2025-03-30 05:57:59 +0600

    Setting up routes for signup, login, and logout functionality.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 const app = express();
 
+import authRoutes from "../src/routes/auth.route.js";
+
+app.use("/api/auth",authRoutes)
+
 app.listen(5001, () => {
   console.log("Server is running on port 5001");
 });
