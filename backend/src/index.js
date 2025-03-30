8b3dcccd3abe7ff3394b2707a26668d885846a32
MirazZim <miraz.zim.38@gmail.com>
2025-03-30 06:18:45 +0600

    Succesfully connected with mongoDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,17 @@
 import express from "express";
-const app = express();
+import dotenv from "dotenv";
+
 
 import authRoutes from "../src/routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 5001;
 app.use("/api/auth",authRoutes)
 
-app.listen(5001, () => {
-  console.log("Server is running on port 5001");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
