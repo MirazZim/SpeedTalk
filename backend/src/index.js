368c5c15bad0707155e7d2876cb43b9ac5f5d850
MirazZim <miraz.zim.38@gmail.com>
2025-03-30 06:40:08 +0600

    For checking purpose

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,6 +12,6 @@ const PORT = process.env.PORT || 5001;
 app.use("/api/auth",authRoutes)
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on portsss ${PORT}`);
   connectDB();
 });
