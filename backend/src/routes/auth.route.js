54da3e4de1c17fb72d82fcc673cb30a44f06099a
MirazZim <miraz.zim.38@gmail.com>
2025-04-02 10:42:15 +0600

    Now i created a account in cloudinary for image. after creating my account i went to dashboard and copied the cloude name then in env file i created that variable. after that i went to cloudinary settings. in api keys i generated new api keys then i stored in my env file. then Export the configured Cloudinary instance for use across the application.

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,6 +2,7 @@ import express from "express";
 const router = express.Router();    
 
 import { signup, login, logout } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 router.post("/signup", signup);
 
