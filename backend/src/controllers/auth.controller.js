733ca14668b9330b39d53a3333771288d58556e3
MirazZim <miraz.zim.38@gmail.com>
2025-04-02 09:56:04 +0600

    feat: implement user login endpoint with authentication and token generation - Retrieve email and password from the request body. - Validate presence of email and password; return 400 if missing. - Search for the user by email in the database. - Verify the provided password using bcryptjs. - Generate a token upon successful authentication. - Return user details and appropriate HTTP status codes for each error case.

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -47,8 +47,37 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.send("Login route");
+export const login = async (req, res) => {
+  //get email and password from request body
+  const { email, password } = req.body;
+  try {
+    if(!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    //find user by email
+    const user = await User.findOne({ email });
+    if(!user) {
+      //if user not found
+      return res.status(400).json({ message: "User not found" });
+    }
+    //compare password in database that if it is correct or not. thats why we use bcryptjs in user model
+    const isPasswordCorrect = await bcryptjs.compare(password, user.password);
+    //if password is not correct
+    if(!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    //generate token
+    generateToken(user._id, res);
+    return res.status(200).json({ 
+      _id: user._id,
+      email: user.email,
+      fullName: user.fullName,
+      profilePic: user.profilePic
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 export const logout = (req, res) => {
