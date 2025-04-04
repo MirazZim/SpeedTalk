cd533474c0901e77ec4029ef3a8259d57b662c23
MirazZim <miraz.zim.38@gmail.com>
2025-04-04 12:35:05 +0600

    Setting up Axios for API requests and global state management with Zustand

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -9,6 +9,8 @@ export default [
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
+      node: true,
+
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,38 @@ import { Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar/Navbar"
 import Footer from "./components/Footer/Footer"
 import HomePage from "./pages/HomePage/HomePage"
+import SignUpPage from "./pages/SignUpPage/SignUpPage"
+import LoginPage from "./pages/LoginPage/LoginPage"
+import SettingsPage from "./pages/SettingsPage/SettingsPage"
+import ProfilePage from "./pages/ProfilePage/ProfilePage"
+import { useAuthStore } from "./store/useAuthStore"
+import { useEffect } from "react"
 
 
 const App = () => {
+  const { authUser, checkAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log("AuthUser: ", authUser);
+
   return (
     <div >
       <Navbar/>
       <Routes>
+        //HomePage
         <Route path="/" element={<HomePage/>} />
+        //SignUpPage
+        <Route path="/signup" element={<SignUpPage/>} />
+        //LoginPage
+        <Route path="/login" element={<LoginPage/>} />
+        //SettingsPage
+        <Route path="/settings" element={<SettingsPage/>} />
+        //ProfilePage
+        <Route path="/profile" element={<ProfilePage/>} />
+
       </Routes>
 
       <Footer/>
