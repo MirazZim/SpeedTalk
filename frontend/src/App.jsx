ce1410c38bc4761fb52363c793c215776267979b
MirazZim <miraz.zim.38@gmail.com>
2025-04-04 12:44:57 +0600

    made our routes protected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import Navbar from "./components/Navbar/Navbar"
 import Footer from "./components/Footer/Footer"
 import HomePage from "./pages/HomePage/HomePage"
@@ -8,10 +8,11 @@ import SettingsPage from "./pages/SettingsPage/SettingsPage"
 import ProfilePage from "./pages/ProfilePage/ProfilePage"
 import { useAuthStore } from "./store/useAuthStore"
 import { useEffect } from "react"
+import { Loader } from "lucide-react"
 
 
 const App = () => {
-  const { authUser, checkAuth } = useAuthStore();
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
   useEffect(() => {
     checkAuth();
@@ -19,20 +20,32 @@ const App = () => {
 
   console.log("AuthUser: ", authUser);
 
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <Loader className="size-10animate-spin"/>
+      </div>
+    );
+  }
+
   return (
     <div >
       <Navbar/>
       <Routes>
         //HomePage
-        <Route path="/" element={<HomePage/>} />
+        <Route path="/" element={authUser ? <HomePage/> : <Navigate to="/login"/>} />
+
         //SignUpPage
-        <Route path="/signup" element={<SignUpPage/>} />
+        <Route path="/signup" element={!authUser ? <SignUpPage/> : <Navigate to="/"/>} />
+        
         //LoginPage
-        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/login" element={!authUser ? <LoginPage/> : <Navigate to="/"/>} />
+        
         //SettingsPage
         <Route path="/settings" element={<SettingsPage/>} />
+        
         //ProfilePage
-        <Route path="/profile" element={<ProfilePage/>} />
+        <Route path="/profile" element={authUser ? <ProfilePage/> : <Navigate to="/login"/>} />
 
       </Routes>
 
