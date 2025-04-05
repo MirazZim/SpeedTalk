d1a567b217f6ce80286f5dc504ad4aa644a60ec9
MirazZim <miraz.zim.38@gmail.com>
2025-04-05 16:03:31 +0600

    side bar is completed not fully but half of it now we are working on chat container. i mean inbox

--- frontend/src/components/ChatContainer/ChatContainer.jsx
+++ frontend/src/components/ChatContainer/ChatContainer.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useChatStore } from '../../store/useChatStore';
+import ChatHeader from '../ChatHeader/ChatHeader';
+import MessageInput from '../MessageInput/MessageInput';
 
 const ChatContainer = () => {
-  return (
-    <div>ChatContainer</div>
-  )
+    const { messages,
+        getMessages,
+        isMessagesLoading,
+        selectedUser,
+        subscribeToMessages,
+        unsubscribeFromMessages, } = useChatStore();
+
+    useEffect(() => {
+        getMessages(selectedUser._id);
+
+       
+    }, [selectedUser._id, getMessages]);
+
+    if (isMessagesLoading) return <div>loading</div>
+
+    return (
+        <div className='flex-1 flex flex-col overflow-auto'>
+            <ChatHeader />
+
+            <p>messages</p>
+
+            <MessageInput />
+
+
+        </div>
+    )
 }
 
 export default ChatContainer
--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -2,11 +2,12 @@ import { useEffect } from 'react'
 import { useChatStore } from '../../store/useChatStore';
 import SidebarSkeleton from '../SidebarSkeleton/SidebarSkeleton';
 import { Users } from 'lucide-react';
+import { useAuthStore } from '../../store/useAuthStore';
 
 const Sidebar = () => {
     const { getUsers, users, selectedUser, setSelectedUser, isUsersLoading } = useChatStore();
+    const { onlineUsers } = useAuthStore();
 
-    const onlineUsers =[];
     useEffect(() => {
         getUsers();
       }, [getUsers]);
