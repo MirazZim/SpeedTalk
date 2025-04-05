ed04c089d65ed92b66ae30a999cef9a72f66b31b
MirazZim <miraz.zim.38@gmail.com>
2025-04-05 16:16:47 +0600

    Implemented the message input and image upload functionality.

--- frontend/src/components/ChatContainer/ChatContainer.jsx
+++ frontend/src/components/ChatContainer/ChatContainer.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useChatStore } from '../../store/useChatStore';
 import ChatHeader from '../ChatHeader/ChatHeader';
 import MessageInput from '../MessageInput/MessageInput';
+import MessageSkeleton from '../MessageSkeleton/MessageSkeleton';
 
 const ChatContainer = () => {
     const { messages,
@@ -17,7 +18,15 @@ const ChatContainer = () => {
        
     }, [selectedUser._id, getMessages]);
 
-    if (isMessagesLoading) return <div>loading</div>
+    if (isMessagesLoading) {
+        return (
+          <div className="flex-1 flex flex-col overflow-auto">
+            <ChatHeader />
+            <MessageSkeleton />
+            <MessageInput />
+          </div>
+        );
+    }
 
     return (
         <div className='flex-1 flex flex-col overflow-auto'>
